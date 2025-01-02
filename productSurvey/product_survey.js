8b0db5e1cc8e4b4756197b741ecc241aa8e48dff
hurtuk18 <adam.hurtuk@gmail.com>
2025-01-02 07:39:40 -0500

    practice task - add userExperince to the feedback

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -8,6 +8,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const userExperience = document.getElementById('userExperience').value;
 
     alert('Thank you for your valuable feedback');
 
@@ -19,6 +20,7 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperienceDisplay').innerHTML = userExperience;
 }
 
 submitButton.onclick = submitFeedback;
